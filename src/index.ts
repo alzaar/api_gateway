56522003540663796cefcf0d77cb37acfa712cee
Ali <alizahir@live.com>
2025-01-27 16:12:54 +0500

    revert express server config to use core node modules

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
 import "dotenv/config";
 import http from "node:http";
 import https from "node:https";
-import express, { Request, Response } from "express";
 
-const app = express();
+const server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    const targetUrl = process.env.PROXY_SERVER_URL || "";
+    const parsedUrl = new URL(targetUrl);
 
-app.get("/", (req: Request, res: Response) => {
-  const targetUrl = process.env.PROXY_SERVER_URL || "";
-  const parsedUrl = new URL(targetUrl);
+    const options = {
+      hostname: parsedUrl.hostname,
+      port: parsedUrl.port || (parsedUrl.protocol === "https" ? 443 : 80),
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    };
 
-  const options = {
-    hostname: parsedUrl.hostname,
-    port: parsedUrl.port || (parsedUrl.protocol === "https" ? 443 : 80),
-    path: req.url,
-    method: req.method,
-    headers: req.headers,
-  };
+    const protocol = parsedUrl.protocol === "https" ? https : http;
 
-  const protocol = parsedUrl.protocol === "https" ? https : http;
+    const proxyReq = protocol.request(
+      options,
+      (proxyRes: http.IncomingMessage) => {
+        const statusCode = proxyRes.statusCode || 500;
+        res.writeHead(statusCode, proxyRes.headers);
+        proxyRes.pipe(res, { end: true });
+      }
+    );
 
-  const proxyReq = protocol.request(
-    options,
-    (proxyRes: http.IncomingMessage) => {
-      const statusCode = proxyRes.statusCode || 500;
-      res.writeHead(statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
-    }
-  );
+    req.pipe(proxyReq, { end: true });
 
-  req.pipe(proxyReq, { end: true });
+    proxyReq.on("error", (err) => {
+      console.error(err);
+      res.writeHead(500, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ message: "Internal server error" }));
+    });
+  }
+);
 
-  proxyReq.on("error", (err) => {
-    console.error(err);
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.write(JSON.stringify({ message: "Internal server error" }));
-  });
-});
-
-app.listen(process.env.PORT, () => console.log("Listening on 8000"));
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on ${process.env.PORT}`)
+);
